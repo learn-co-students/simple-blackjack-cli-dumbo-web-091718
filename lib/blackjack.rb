c4c71efe351c09e4e8de958c9a2d8b482e3f0ab1
scott.waring@gmail.com <scott.waring@gmail.com>
2018-08-11 19:29:48 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,44 +1,81 @@
+$card = []
+
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  return rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(number = $card[0])
+  puts  "Your cards add up to #{number}"
 end
 
-def prompt_user
-  # code #prompt_user here
+def prompt_user(number = $card[0])
+    puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  question = gets.chomp
+  return question
 end
 
-def end_game
-  # code #end_game here
+def end_game(number = $card[0])
+    return puts  "Sorry, you hit #{number}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  card1 = deal_card()
+  card2 = deal_card()
+  card_total = card1 + card2
+  display_card_total(card_total)
+  $card << card_total
+  return $card[0]
 end
 
-def hit?
-  # code hit? here
+def hit?(number = $card[0])
+  new_card = deal_card
+  prompt_user
+  hand = get_user_input
+
+  if "h" === hand || "H" === hand
+    $card[0] += new_card 
+    if $card[0] > 21
+      return end_game
+    else
+      display_card_total
+      hit?
+    end
+  elsif "s" === hand || "S" === hand 
+    return $card[0]
+  else
+    return invalid_command
+  end
+
+  return $card[0]
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
+  return hit?
 end
+
 
 #####################################################
 # get every test to pass before coding runner below #
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  initial_round
+  total = $card[0]
+  if total < 21
+      hit?  
+  else
+    return end_game
+  end
 end
-    
+
+runner
+
