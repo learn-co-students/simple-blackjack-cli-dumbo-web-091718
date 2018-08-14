4eaa810a95043a3d66c4c53a3c657ad230129c14
hsadoqi@gmail.com <hsadoqi@gmail.com>
2018-08-14 01:27:48 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -62,5 +62,12 @@ end
 
 def runner
   # code runner here
+  welcome
+  total = initial_round
+  total = hit? (total)
+  display_card_total(total)
+  until total > 21
+  end
+  end_game (total)
 end
     
